ea681a2ebc9ce7e05c62489776e388fa07d2ac77
zivuch <zivuch@gmail.com>
2025-04-29 09:48:30 +0300

    expand true

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ import express from "express";
 import { join, resolve } from "path";
 
 const app = express();
-const __dirname = resolve()
+const __dirname = resolve();
 
 console.log(join(__dirname, "dist", "index.html"));
 
 app.use(express.static(join(__dirname, "dist")));
-app.get("/*any", (_, res) => res.sendFile(join(__dirname, "dist", "index.html")));
 
-app.listen(process.env.PORT || 3001, () => console.log("🚀 Server ready"));
+app.get("/*any", (_, res) => {
+  res.sendFile(join(__dirname, "dist", "index.html"));
+});
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("Server run on " + process.env.PORT || 3001);
+});
